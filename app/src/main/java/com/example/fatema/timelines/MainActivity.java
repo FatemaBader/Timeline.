3591f0712955a2e3ba50bd98c3bbfd9438405201
fatema <fatema.bader@sap.com>
2018-11-14 12:59:00 +0000

    Retrieving photo from database

--- app/src/main/java/com/example/fatema/timelines/MainActivity.java
+++ app/src/main/java/com/example/fatema/timelines/MainActivity.java
@@ -1,75 +1,31 @@
 package com.example.fatema.timelines;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.io.IOException;
+import java.util.UUID;
 
-/*
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    private static final int RESULT_LOAD_IMAGE = 1;
-
-    ImageView imageToUplaod, downloadedImage;
-    Button bUploadImage, bDownloadImage;
-    EditText uploadImageName, downloadImageName;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        imageToUplaod  = (ImageView) findViewById(R.id.imageToUpload);
-        downloadedImage = (ImageView) findViewById(R.id.downloadedImage);
-
-        bUploadImage = (Button) findViewById(R.id.bUploadImage);
-        bDownloadImage = (Button) findViewById(R.id.bDownloadImage);
-
-        uploadImageName = (EditText) findViewById(R.id.etUploadName);
-        downloadImageName = (EditText) findViewById(R.id.etDownloadName);
-
-        imageToUplaod.setOnClickListener(this);
-        bUploadImage.setOnClickListener(this);
-        bDownloadImage.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View v){
-        switch(v.getId()){
-            case R.id.imageToUpload:
-                //opens the gallery
-                Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
-                break;
-            case R.id.bUploadImage:
-                break;
-            case R.id.bDownloadImage:
-                break;
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        //after user selects picture from gallery
-        //make sure gallery is called
-        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null)
-        {
-            //shows address of selected image in the phone
-            Uri selectedImage = data.getData();
-            //displays the image
-            imageToUplaod.setImageURI(selectedImage);
-        }
-    }
-}*/
 
 public class MainActivity extends AppCompatActivity {
 
@@ -80,10 +36,18 @@ public class MainActivity extends AppCompatActivity {
 
     private final int PICK_IMAGE_REQUEST =71;
 
+    //FIREBASE STORAGE
+    FirebaseStorage storage;
+    StorageReference storageReference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //FireBase init
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
 
         //init view
         btnChoose = (Button) findViewById(R.id.btnChoose);
@@ -99,10 +63,48 @@ public class MainActivity extends AppCompatActivity {
         btnUpload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                uploadImage();
             }
         });
     }
+
+    private void uploadImage() {
+        if (filePath!= null)
+        {
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Uploading..");
+            progressDialog.show();
+
+            StorageReference ref = storageReference.child("images/" +UUID.randomUUID().toString());
+            ref.putFile(filePath)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            progressDialog.dismiss();
+                            Toast.makeText(MainActivity.this,"Uploaded",Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            progressDialog.dismiss();
+                            Toast.makeText(MainActivity.this,"Failed "+e.getMessage(),Toast.LENGTH_SHORT).show();
+
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100.0*taskSnapshot.getBytesTransferred()/taskSnapshot.getTotalByteCount());
+                            progressDialog.setMessage("Uplaoded" +(int)progress+"%");
+                        }
+                    });
+
+
+
+        }
+    }
+
     private void chooseImage() {
         Intent intent = new Intent();
         intent.setType("image/*");
